09221e718d184693466f0ddee52c684da37825b6
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 16:37:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 
-# 创建文件上传器
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is not None:
-    # 读取文件内容
-    bytes_data = uploaded_file.getvalue()
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
-
-    # 如果是文本文件，可以解码成字符串
-    if uploaded_file.type == "text/plain":
-        string_data = bytes_data.decode('utf-8')
-        st.write(string_data)
