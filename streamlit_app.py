a97d5bc883ce4cd119ba4e868754943771a0a40a
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 16:13:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
+import os
 import streamlit as st
-import pandas as pd
 
-st.set_page_config(page_title="数据后处理过程演示", layout="wide")
-config_file_path = st.text_input('输入配置文件路径', help='json', )
-if st.button("读取配置文件"):
-    json_file = pd.read_csv(config_file_path)
-    st.write(json_file.head())
+# 获取环境变量
+file_path = os.getenv('FILE_PATH')
+
+if file_path:
+    try:
+        # 读取文件内容
+        with open(file_path, 'r') as file:
+            content = file.read()
+        
+        # 在Streamlit页面中显示内容
+        st.write(content)
+    except FileNotFoundError:
+        st.error(f"File not found: {file_path}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+else:
+    st.error("FILE_PATH environment variable not set")
