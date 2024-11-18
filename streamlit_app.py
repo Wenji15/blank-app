188a83634df4678dae3b434abf079c46a2be19e0
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 17:06:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ if uploaded_file is not None:
     # st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    stringio = StringIO(uploaded_file.getvalue().decode("gkb"))
     # st.write(stringio)
 
     # To read file as string:
